042771810a345c3ffe2925b921dd5de89c69d511
ally-macgregor-sonarsource <ally.macgregor@sonarsource.com>
2023-03-03 15:15:30 +0100

    Fixed errors

--- src/main/java/com/portal/openshiftjenkins/controller/ApplicationFeatures.java
+++ src/main/java/com/portal/openshiftjenkins/controller/ApplicationFeatures.java
@@ -3,11 +3,7 @@ package com.portal.openshiftjenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class ApplicationFeatures {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationFeatures.class, args);
 
         //TODO: Review this!
 		if ( 1 == 1)
